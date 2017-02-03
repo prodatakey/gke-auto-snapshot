64f3d0ed9a7fb39d3aba4e326ad32b6445772c4d
Gatis Tomsons <gatis.tomsons@eazybi.com>
2017-02-03 09:41:45 +0200

    Add support for BSD date command
    Without this users can't run this on osx or bsd systems.

--- google-cloud-auto-snapshot.sh
+++ google-cloud-auto-snapshot.sh
@@ -8,7 +8,12 @@ done
 # snapshots are incremental and dont need to be deleted, deleting snapshots will merge snapshots, so deleting doesn't loose anything
 # having too many snapshots is unwiedly so this script deletes them after 60 days
 #
-gcloud compute snapshots list --filter="creationTimestamp<$(date -d "-60 days" "+%Y-%m-%d")" --regexp "(autogcs.*)" --uri | while read SNAPSHOT_URI; do
+if [[ $(uname) == "Linux" ]]; then
+  from_date=$(date -d "-60 days" "+%Y-%m-%d")
+else
+  from_date=$(date -v -60d "+%Y-%m-%d")
+fi
+gcloud compute snapshots list --filter="creationTimestamp<$from_date" --regexp "(autogcs.*)" --uri | while read SNAPSHOT_URI; do
    gcloud compute snapshots delete $SNAPSHOT_URI
 done
 #
